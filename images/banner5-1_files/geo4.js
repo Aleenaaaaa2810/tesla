e24ba01f0dc93c4627a91166990636b118240325
Aleenaaaaa2810 <aleena123saju@gmail.com>
2024-07-09 21:51:25 +0530

    Add files via upload

---
+++ images/banner5-1_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "KOCHI",
+      'continent': "AS",
+      'country': "IN",
+      'region': "KL",
+      'dma': ""
+    },
+    'ip':"106.222.176.213"
+  }]);
+})
+
+()
+
+;
